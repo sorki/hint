0eafd53bba7fa84b2240d55c8e30ea0675274134
Brandon Chinn <brandon@leapyear.io>
2021-11-12 11:50:46 -0800

    Update GHC imports + shims

--- src/Hint/Annotations.hs
+++ src/Hint/Annotations.hs
@@ -9,13 +9,20 @@ import GHC.Serialized
 import Hint.Base
 import qualified Hint.GHC as GHC
 
-#if MIN_VERSION_ghc(9,0,0)
+#if MIN_VERSION_ghc(9,2,0)
+import GHC (ms_mod)
+import GHC.Driver.Env (hsc_mod_graph)
+#elif MIN_VERSION_ghc(9,0,0)
 import GHC.Driver.Types (hsc_mod_graph, ms_mod)
+#else
+import HscTypes (hsc_mod_graph, ms_mod)
+#endif
+
+#if MIN_VERSION_ghc(9,0,0)
 import GHC.Types.Annotations
 import GHC.Utils.Monad (concatMapM)
 #else
 import Annotations
-import HscTypes (hsc_mod_graph, ms_mod)
 import MonadUtils (concatMapM)
 #endif
 
--- src/Hint/GHC.hs
+++ src/Hint/GHC.hs
@@ -10,45 +10,79 @@ module Hint.GHC (
 import GHC as X hiding (Phase, GhcT, runGhcT)
 import Control.Monad.Ghc as X (GhcT, runGhcT)
 
-#if MIN_VERSION_ghc(9,0,0)
+#if MIN_VERSION_ghc(9,2,0)
+import GHC.Types.SourceError as X (SourceError, srcErrorMessages)
+
+import GHC.Driver.Ppr as X (showSDoc)
+
+import GHC.Types.SourceFile as X (HscSource(HsSrcFile))
+
+import GHC.Utils.Logger as X (LogAction)
+import GHC.Platform.Ways as X (Way (..))
+
+import GHC.Types.TyThing.Ppr as X (pprTypeForUser)
+#elif MIN_VERSION_ghc(9,0,0)
 import GHC.Driver.Types as X (SourceError, srcErrorMessages, GhcApiError)
 
+import GHC.Utils.Outputable as X (showSDoc, showSDocForUser, showSDocUnqual)
+
+import GHC.Utils.Error as X (pprErrMsgBagWithLoc)
+
+import GHC.Driver.Phases as X (HscSource(HsSrcFile))
+
+import GHC.Parser.Lexer as X (mkPState)
+
+import GHC.Driver.Session as X (LogAction, addWay')
+import GHC.Driver.Ways as X (Way (..))
+
+import GHC.Core.Ppr.TyThing as X (pprTypeForUser)
+#else
+import HscTypes as X (SourceError, srcErrorMessages, GhcApiError)
+
+import Outputable as X (showSDoc, showSDocForUser, showSDocUnqual)
+
+import ErrUtils as X (pprErrMsgBagWithLoc)
+
+import DriverPhases as X (HscSource(HsSrcFile))
+
+import Lexer as X (mkPState)
+
+import DynFlags as X (LogAction, addWay', Way(..))
+
+import PprTyThing as X (pprTypeForUser)
+#endif
+
+#if MIN_VERSION_ghc(9,0,0)
 import GHC.Utils.Outputable as X (PprStyle, SDoc, Outputable(ppr),
-                                  showSDoc, showSDocForUser, showSDocUnqual,
                                   withPprStyle, defaultErrStyle, vcat)
 
-import GHC.Utils.Error as X (mkLocMessage, pprErrMsgBagWithLoc, errMsgSpan)
+import GHC.Utils.Error as X (mkLocMessage, errMsgSpan)
 
-import GHC.Driver.Phases as X (Phase(Cpp), HscSource(HsSrcFile))
+import GHC.Driver.Phases as X (Phase(Cpp))
 import GHC.Data.StringBuffer as X (stringToStringBuffer)
-import GHC.Parser.Lexer as X (P(..), ParseResult(..), mkPState,
+import GHC.Parser.Lexer as X (P(..), ParseResult(..),
                               getErrorMessages)
 import GHC.Parser as X (parseStmt, parseType)
 import GHC.Data.FastString as X (fsLit)
 
-import GHC.Driver.Session as X (xFlags, xopt, LogAction, FlagSpec(..),
-                                WarnReason(NoReason), addWay')
-import GHC.Driver.Ways as X (Way (..))
+import GHC.Driver.Session as X (xFlags, xopt, FlagSpec(..), WarnReason(NoReason))
 
-import GHC.Core.Ppr.TyThing as X (pprTypeForUser)
 import GHC.Types.SrcLoc as X (combineSrcSpans, mkRealSrcLoc)
 
 import GHC.Core.ConLike as X (ConLike(RealDataCon))
 #else
-import HscTypes as X (SourceError, srcErrorMessages, GhcApiError)
 import HscTypes as X (mgModSummaries)
 
 import Outputable as X (PprStyle, SDoc, Outputable(ppr),
-                        showSDoc, showSDocForUser, showSDocUnqual,
                         withPprStyle, defaultErrStyle, vcat)
 
-import ErrUtils as X (mkLocMessage, pprErrMsgBagWithLoc
+import ErrUtils as X (mkLocMessage
 #if __GLASGOW_HASKELL__ >= 810
   , errMsgSpan
 #endif
   )
 
-import DriverPhases as X (Phase(Cpp), HscSource(HsSrcFile))
+import DriverPhases as X (Phase(Cpp))
 import StringBuffer as X (stringToStringBuffer)
 import Lexer as X (P(..), ParseResult(..), mkPState
 #if __GLASGOW_HASKELL__ >= 810
@@ -58,10 +92,8 @@ import Lexer as X (P(..), ParseResult(..), mkPState
 import Parser as X (parseStmt, parseType)
 import FastString as X (fsLit)
 
-import DynFlags as X (xFlags, xopt, LogAction, FlagSpec(..),
-                      WarnReason(NoReason), addWay', Way(..))
+import DynFlags as X (xFlags, xopt, FlagSpec(..), WarnReason(NoReason))
 
-import PprTyThing as X (pprTypeForUser)
 import SrcLoc as X (combineSrcSpans, mkRealSrcLoc)
 
 import ConLike as X (ConLike(RealDataCon))
@@ -69,7 +101,10 @@ import ConLike as X (ConLike(RealDataCon))
 
 {-------------------- Imports for Shims --------------------}
 
-#if MIN_VERSION_ghc(9,0,0)
+#if MIN_VERSION_ghc(9,2,0)
+-- dynamicGhc
+import GHC.Platform.Ways (hostIsDynamic)
+#elif MIN_VERSION_ghc(9,0,0)
 -- dynamicGhc
 import GHC.Driver.Ways (hostIsDynamic)
 
@@ -94,7 +129,11 @@ dynamicGhc = GHC.dynamicGhc
 #endif
 
 -- Message
+#if MIN_VERSION_ghc(9,2,0)
+type Message = SDoc
+#else
 type Message = GHC.MsgDoc
+#endif
 
 -- Logger
 type Logger = LogAction
